a58c54a910c44b76279bc70d7852f19ff0d2d6a9
elvfor <elvfor-0@student.ltu.se>
2024-04-12 16:06:04 +0200

    some work on details screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     implementation(libs.coil.compose)
-    //implementation(libs.androidx.navigation.runtime.ktx)
-    //implementation(libs.androidx.navigation.compose)
+    //noinspection UseTomlInstead
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
