dd66e29205cec517d76065c3412f9f063d6ea730
jennysundstroem <ejyuso-2@student.ltu.se>
2024-05-09 08:18:05 +0200

    not working yet

--- app/src/main/java/com/ltu/m7019e/miniproject/countries/ui/screens/CountryListScreen.kt
+++ app/src/main/java/com/ltu/m7019e/miniproject/countries/ui/screens/CountryListScreen.kt
@@ -15,9 +15,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -80,7 +83,21 @@ fun CountryListScreen(
                     }
                 }
 
-                CountryListUiState.NoNetwork -> TODO()
+                CountryListUiState.NoNetwork ->
+                    item {
+                        Column {
+                            Icon(
+                                imageVector = Icons.Default.Warning,
+                                contentDescription = "No Internet Connection",
+                                modifier = Modifier.size(200.dp)
+                            )
+                            Text(
+                                text = "No Network Connection!",
+                                style = MaterialTheme.typography.bodySmall,
+                                modifier = Modifier.padding(16.dp)
+                            )
+                        }
+                    }
             }
         }
     }
