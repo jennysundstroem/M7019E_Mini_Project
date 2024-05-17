e7f3dda72e4a05cbe58ba8aa7d626468da901191
jennysundstroem <ejyuso-2@student.ltu.se>
2024-05-17 11:53:37 +0200

    theme done

--- app/src/main/java/com/ltu/m7019e/miniproject/countries/ui/theme/Color.kt
+++ app/src/main/java/com/ltu/m7019e/miniproject/countries/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.ltu.m7019e.miniproject.countries.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFFFFCDD2)
+val md_theme_light_primary = Color(0xFFB71C1C)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
 val md_theme_light_primaryContainer = Color(0xFFB71C1C)
 val md_theme_light_onPrimaryContainer = Color(0xFFFFEBEE)
